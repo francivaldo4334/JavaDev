9d8a2a0e3a3d560250e9d809de8f2c6de7f7f5d7
francivaldo4334 <francivaldodev@gmail.com>
2023-01-28 11:44:29 -0300

    adiciona janela

---
+++ test/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame win = new JFrame();
+    win.setSize(700, 700);
+    win.setVisible(true);
+  }
+}
